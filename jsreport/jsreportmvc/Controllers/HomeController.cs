f57a49a49c03af6bd000b41f0d22602caf4697b9
Mike Breault <mbreault@users.noreply.github.com>
2022-07-26 16:52:06 -0500

    jsreport

---
+++ jsreport/jsreportmvc/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using jsreportmvc.Models;
+using jsreport.AspNetCore;
+using jsreport.Types;
+
+namespace jsreportmvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    public IJsReportMVCService JsReportMVCService { get; }
+
+    public HomeController(ILogger<HomeController> logger, IJsReportMVCService jsReportMVCService)
+    {
+        _logger = logger;
+        JsReportMVCService = jsReportMVCService;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [MiddlewareFilter(typeof(JsReportPipeline))]
+    public IActionResult Report()
+    {
+        HttpContext.JsReportFeature().Recipe(Recipe.ChromePdf);
+
+        return View();
+
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ jsreport/jsreportmvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace jsreportmvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
